15b088c599a7255977cb488b68b3f594296a0653
ggorockee <ggorockee@gmail.com>
2025-09-22 02:33:53 +0900

    style: 디버그 배너 제거

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -70,7 +70,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: AppConfig.appName,
-      debugShowCheckedModeBanner: !AppConfig.isProduction,
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const SplashScreen(),
       onGenerateRoute: (settings) {
