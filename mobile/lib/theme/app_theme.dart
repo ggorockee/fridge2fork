220f8a6290f92a663bda8787bea06c28e60e7e75
ggorockee <ggorockee@gmail.com>
2025-10-05 15:57:09 +0900

    style(mobile/theme): spacingM 값 16.0에서 64.0으로 확대
    중간 크기 간격을 더 넓게 조정하여 UI 간격 개선

--- mobile/lib/theme/app_theme.dart
+++ mobile/lib/theme/app_theme.dart
@@ -49,7 +49,7 @@ class AppTheme {
   // 📏 Spacing System
   static const double spacingXS = 4.0;
   static const double spacingS = 8.0;
-  static const double spacingM = 16.0;
+  static const double spacingM = 64.0;
   static const double spacingL = 24.0;
   static const double spacingXL = 32.0;
   static const double spacingXXL = 48.0;
